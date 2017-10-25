1777600e7c7655e1d9766e560566fde52c02e50b
tz1006 <a@omg.tf>
2017-10-24 22:44:18 -0700

    Update ufw.sh

--- ufw.sh
+++ ufw.sh
@@ -4,9 +4,9 @@ sudo apt-get update -y
 sudo apt-get install -y ufw
 # sudo apt-get install gufw
 
-# sudo sed -i 's/^net\/ipv4\/conf\/all\/accept_redirects.*/net\/ipv4\/conf\/all\/accept_redirects=1/g' /etc/ufw/sysctl.conf
 sudo sed -i 's/^IPV6.*/IPV6=no/g' /etc/default/ufw
 sudo sed -i 's/^IPT_SYSCTL.*/IPT_SYSCTL=\/etc\/sysctl.conf/g' /etc/default/ufw
+sudo sed -i 's/^net\/ipv4\/conf\/all\/accept_redirects.*/net\/ipv4\/conf\/all\/accept_redirects=1/g' /etc/ufw/sysctl.conf
 # sudo sed -i 's/^net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 
 sudo ufw default deny incoming
@@ -17,10 +17,16 @@ sudo ufw allow 22
 sudo ufw allow 53
 sudo ufw allow 80
 sudo ufw allow 443
+sudo ufw allow 500
+sudo ufw allow 4500
+sudo ufw allow 1701
+sudo ufw allow 1723
 
 sudo ufw logging on
 sudo ufw disable && sudo ufw --force enable
 # sudo ufw reset
+# https://gist.github.com/kimus/9315140
+# https://www.darrenfang.com/2014/01/install-pptp-vpn-on-ubuntu/
 # https://www.polarxiong.com/archives/ubuntu-ufw-vpn-can-not-connect-internet.html
 # https://www.peterdavehello.org/2016/01/ubuntu-based-gnulinux-firewall-ufw-essential-config/
 # https://www.digitalocean.com/community/tutorials/how-to-setup-a-firewall-with-ufw-on-an-ubuntu-and-debian-cloud-server
