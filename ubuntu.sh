299e913ac4982fb200ec3bc6e2f24d263d7a3456
tz1006 <a@omg.tf>
2017-11-07 13:43:43 -0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,19 +2,19 @@
 sudo apt-get update -y
 # sudo apt-get install -y git sysv-rc-conf
 
-git clone https://github.com/tz1006/PPTP-IPsec.git
+https://github.com/tz1006/Ubuntu-VPN.git
 
 # Install PPTP
 sudo apt-get install -y pptpd
-sudo cp -f ~/PPTP-IPsec/options.pptpd /etc/ppp/options.pptpd
-sudo cp -f ~/PPTP-IPsec/chap-secrets /etc/ppp/chap-secrets
-sudo cp -f ~/PPTP-IPsec/pptpd.conf /etc/pptpd.conf
+sudo cp -f ~/Ubuntu-VPN/options.pptpd /etc/ppp/options.pptpd
+sudo cp -f ~/Ubuntu-VPN/chap-secrets /etc/ppp/chap-secrets
+sudo cp -f ~/Ubuntu-VPN/pptpd.conf /etc/pptpd.conf
 
 # Install IPsec
 sudp apt-get install -y strongswan strongswan-plugin-xauth-generic
-sudo cp -f ~/PPTP-IPsec/ipsec.conf /etc/strongswan/ipsec.conf
-sudo cp -f ~/PPTP-IPsec/ipsec.secrets /etc/strongswan/ipsec.secrets
-sudo cp -f ~/PPTP-IPsec/strongswan.conf /etc/strongswan/strongswan.conf
+sudo cp -f ~/Ubuntu-VPN/ipsec.conf /etc/strongswan/ipsec.conf
+sudo cp -f ~/Ubuntu-VPN/ipsec.secrets /etc/strongswan/ipsec.secrets
+sudo cp -f ~/Ubuntu-VPN/strongswan.conf /etc/strongswan/strongswan.conf
 
 # Sysctl Config
 # /sbin/iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
