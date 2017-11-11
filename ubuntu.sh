41121000380371b19663217ac52f8e7ea6b8c7f4
tz1006 <a@omg.tf>
2017-11-11 11:40:30 -0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -6,16 +6,20 @@ sudo apt-get -y install git
 git clone https://github.com/tz1006/Ubuntu-VPN.git
 
 # Install PPTP
-sudo apt-get install -y pptpd
-sudo cp -f ~/Ubuntu-VPN/pptp/options.pptpd /etc/ppp/options.pptpd
-sudo cp -f ~/Ubuntu-VPN/pptp/chap-secrets /etc/ppp/chap-secrets
-sudo cp -f ~/Ubuntu-VPN/pptp/pptpd.conf /etc/pptpd.conf
+#sudo apt-get install -y pptpd
+#sudo cp -f ~/Ubuntu-VPN/pptp/pptpd.conf /etc/pptpd.conf
+#sudo cp -f ~/Ubuntu-VPN/pptp/options.pptpd /etc/ppp/options.pptpd
+#sudo cp -f ~/Ubuntu-VPN/pptp/chap-secrets /etc/ppp/chap-secrets
 
 # Install IPsec
 sudo apt-get install -y strongswan strongswan-plugin-xauth-generic
 sudo cp -f ~/Ubuntu-VPN/strongswan/ipsec.conf /etc/ipsec.conf
 sudo cp -f ~/Ubuntu-VPN/strongswan/ipsec.secrets /etc/ipsec.secrets
 sudo cp -f ~/Ubuntu-VPN/strongswan/strongswan.conf /etc/strongswan.conf
+
+# Install sock5
+sudo apt-get install -y danted-server
+sudo cp -f ~/Ubuntu-VPN/danted/danted.conf /etc/danted.conf
 
 # Sysctl Config
 # /sbin/iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
@@ -25,10 +29,10 @@ sudo cp -f ~/Ubuntu-VPN/strongswan/strongswan.conf /etc/strongswan.conf
 # ufw
 sudo bash ~/Ubuntu-VPN/ufw.sh
 # Start
-sudo service pptpd restart
-
+#sudo service pptpd restart
 sudo service strongswan restart
-sudo ipsec restart
+#sudo ipsec restart
+sudo service danted restart
 
 # http://www.jianshu.com/p/0588a87feff6
 # https://www.darrenfang.com/2014/01/install-pptp-vpn-on-ubuntu/
